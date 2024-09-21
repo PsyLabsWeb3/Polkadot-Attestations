eec3724adc65dcb87d603fedd9b6c851f4c3ff3d
TerratekMusic <35459364+TerratekMusic@users.noreply.github.com>
2024-09-21 16:01:09 -0600

    connect wallet imp-1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,21 @@
-import  { useEffect, useState } from 'react'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
 
 //POLKADOT API
-import { ApiPromise, WsProvider } from '@polkadot/api';
-import Home from './components/pages/Home';
-import { Box, Container, Flex } from '@chakra-ui/react';
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import Home from "./components/pages/Home";
+import { Flex } from "@chakra-ui/react";
 
 function App() {
+  const [api, setApi] = useState<any>(null);
 
-const [api, setApi] = useState<any>(null);
-
-const setupApi = async () => {
-    const wsProvider = new WsProvider('ws://127.0.0.1:9944');
+  const setupApi = async () => {
+    //POLKADOT ASSETS HUB RPC FOR NON LOCAL DEVELOPMENT UNTIL WE HAVE OUR OWN NODE
+    const wsProvider = new WsProvider(
+      "wss://polkadot-asset-hub-rpc.polkadot.io"
+    );
+    //USE THIS FOR LOCAL DEVELOPMENT WITH THE NODE RUNNING
+    // const wsProvider = new WsProvider("ws://127.0.0.1:9944");
     const api = await ApiPromise.create({ provider: wsProvider });
     setApi(api);
 
@@ -19,39 +23,37 @@ const setupApi = async () => {
     const [chain, nodeName, nodeVersion] = await Promise.all([
       api.rpc.system.chain(),
       api.rpc.system.name(),
-      api.rpc.system.version()
+      api.rpc.system.version(),
     ]);
 
-    console.log(`You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`);
+    console.log(
+      `You are connected to chain ${chain} using ${nodeName} v${nodeVersion}`
+    );
+  };
 
-   
-}
+  useEffect(() => {
+    setupApi();
+  }, []);
 
-useEffect(() => {
-  setupApi();
-}
-, []);
+  useEffect(() => {
+    if (api) {
+      console.log("PolkAttest is connected");
+      console.log(api);
 
-useEffect(() => {
-  if(api) {
-    console.log('PolkAttest is connected');
-    console.log(api);
+      const getBlock = async () => {
+        const block = await api.query.timestamp.now();
+        console.log("Block:", block.toPrimitive());
+      };
 
-    const getBlock = async () => {
-      const block = await api.query.timestamp.now();
-      console.log('Block:', block.toPrimitive());
+      getBlock();
     }
-
-    getBlock();
-  }
-
-  }), [api];
+  }, [api]);
 
   return (
-    <Flex w="100%" h="100%">    
-    <Home />
+    <Flex w="100%" h="100%">
+      <Home />
     </Flex>
-  )
+  );
 }
 
-export default App
+export default App;
--- frontend/src/components/templates/Header/Header.tsx
+++ frontend/src/components/templates/Header/Header.tsx
@@ -1,11 +1,89 @@
-import { Link, Flex, Button } from "@chakra-ui/react";
+import { useState } from "react";
+import { Link, Flex, Button, Select } from "@chakra-ui/react";
+import { web3Accounts, web3Enable } from "@polkadot/extension-dapp";
 
-function Header () {
+const NAME = "PolkAttest";
+
+function Header() {
+  const [allAccounts, setAllAccounts] = useState<any>([]);
+  const [selectedAccount, setSelectedAccount] = useState<any>(null);
+
+  const handleConnectWallet = async () => {
+    const extensions = await web3Enable(NAME);
+
+    if (!extensions) {
+      throw new Error("No wallet installed found");
+    }
+
+    const localAccounts = await web3Accounts();
+    setAllAccounts(localAccounts);
+
+    if (localAccounts.length === 1) {
+      setSelectedAccount(localAccounts[0]);
+      console.log("Selected Account", selectedAccount);
+    }
+
+    console.log(allAccounts);
+  };
+
+  const handleSelectAccount = (account: any) => {
+    setSelectedAccount(account);
+  };
+
+  //SLICE ACCOUNT TO SHOW ONLY LAST 4 DIGITS AND # POINTS
+  const formatAccount = (account: string) => {
+    if (account && account.length > 4) {
+      return `...${account.slice(-4)}`;
+    }
+    return account;
+  };
+
+  console.log("Selected Account", selectedAccount);
+
   return (
-    <Flex as="header" w="100%s" align="center" justify="space-between" wrap="wrap" padding="1.5rem" bg="white" color="black">
-      <Link href="/" fontSize="2xl" fontWeight="bold">PolkAttest</Link>
-      <Link href="/" fontSize="2xl" fontWeight="bold">PolkAttest</Link>
-      <Button >Connect Wallet</Button>
+    <Flex
+      as="header"
+      w="100%s"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding="1.5rem"
+      bg="white"
+      color="black"
+    >
+      <Link href="/" fontSize="2xl" fontWeight="bold">
+        PolkAttest
+      </Link>
+      <Link href="/" fontSize="2xl" fontWeight="bold">
+        PolkAttest
+      </Link>
+      {allAccounts.length === 0 ? (
+        <Button onClick={handleConnectWallet}>Connect Wallet</Button>
+      ) : null}
+      {allAccounts.length === 1 && selectedAccount ? (
+        <Button onClick={handleConnectWallet}>
+          {formatAccount(selectedAccount.address)}
+        </Button>
+      ) : (
+        <></>
+      )}
+      {allAccounts.length > 0 && !selectedAccount ? (
+        <>
+          <Select placeholder="Select Account">
+            {allAccounts.map((account: any) => (
+              <option
+                key={account.address}
+                value={account.address}
+                onClick={() => handleSelectAccount(account)}
+              >
+                {formatAccount(account.address)}
+              </option>
+            ))}
+          </Select>
+        </>
+      ) : (
+        <></>
+      )}
     </Flex>
   );
 }
