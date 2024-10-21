2914bba638b407b80c23d08ce13a591f3bb8e4d1
Luchex <lucianog2000@gmail.com>
2024-10-21 18:32:42 -0300

    [FEATURE][POL-22]: Adding IPFS

--- frontend/src/components/pages/Attest.tsx
+++ frontend/src/components/pages/Attest.tsx
@@ -23,10 +23,11 @@ import {
   SchemaData,
 } from "../contexts/ApiContext";
 import { useWallet } from "../contexts/AccountContext";
+import { PinataManager } from "../../utils/pinataManager";
 
-type FormValues = {
-  [key: string]: string | number | boolean;
-};
+type FormValues = { [key: string]: string | number | boolean };
+
+const pinataManager = new PinataManager(import.meta.env.VITE_PINATA_JWT || "");
 
 function Attest() {
   const { id } = useParams<{ id: string }>();
@@ -34,6 +35,8 @@ function Attest() {
   const [fields, setFields] = useState<FormValues>({ subject: "" });
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  const [uploading, setUploading] = useState<boolean>(false);
+  const [uploadedFileCIDs, setUploadedFileCIDs] = useState<string[]>([]);
 
   const { selectedAccount } = useWallet();
   const {
@@ -79,13 +82,29 @@ function Attest() {
     fieldName: string
   ) => {
     const { value } = e.target;
-    // Regular expression to disallow special characters and accents
+
     const regex = /^[a-zA-Z0-9 ]*$/;
     if (regex.test(value)) {
       setFields((prevValues) => ({
         ...prevValues,
         [fieldName]: value,
       }));
+    }
+  };
+
+  const handleFileUpload = async (file: File, fieldName: string) => {
+    try {
+      setUploading(true);
+
+      const cid = await pinataManager.pinFile(file, { name: file.name });
+
+      setFields((prev) => ({ ...prev, [fieldName]: cid }));
+
+      setUploadedFileCIDs((prevCIDs) => [...prevCIDs, cid]);
+    } catch (error) {
+      console.error("Error uploading file:", error);
+    } finally {
+      setUploading(false);
     }
   };
 
@@ -100,16 +119,11 @@ function Attest() {
       return;
     }
 
-    // Ensure all fields are filled
-    for (const key in fields) {
-      if (
-        fields[key] === "" ||
-        fields[key] === null ||
-        fields[key] === undefined
-      ) {
-        setErrorMessage("All fields are required.");
-        return;
-      }
+    if (
+      Object.values(fields).some((value) => value === "" || value === undefined)
+    ) {
+      setErrorMessage("All fields are required.");
+      return;
     }
 
     const attestationData: SchemaField[] =
@@ -133,10 +147,21 @@ function Attest() {
         api.tx.attestations.insertAttestation,
         [attestation]
       );
-      setSuccessMessage(`Transaction successful!`);
+      setSuccessMessage("Transaction successful!");
+
+      setUploadedFileCIDs([]);
     } catch (error) {
       console.error("Error inserting attestation:", error);
       setErrorMessage(`Failed to insert attestation: ${error}`);
+
+      for (const cid of uploadedFileCIDs) {
+        try {
+          await pinataManager.unpinFile(cid);
+          console.log(`File with CID ${cid} unpinned successfully.`);
+        } catch (unpinError) {
+          console.error(`Error unpinning file with CID ${cid}:`, unpinError);
+        }
+      }
     }
   };
 
@@ -161,7 +186,7 @@ function Attest() {
             alignItems="center"
             justifyContent="center"
             textAlign="center"
-            height="200px"
+            height="fit-content"
             width="400px"
             borderRadius="md"
             boxShadow="lg"
@@ -224,54 +249,122 @@ function Attest() {
                     placeholder="Enter subject"
                     value={fields.subject as string}
                     onChange={(e) => handleChange(e, "subject")}
+                    borderColor="gray.500"
+                    bg="white"
+                    _placeholder={{ color: "gray.500" }}
+                    _focus={{ borderColor: "brand.primary" }}
+                    _focusVisible={{ outline: "none" }}
                   />
                 </FormControl>
 
-                {schema.fields.map((field: SchemaField) => (
-                  <FormControl key={field.name} mb={4} isRequired>
-                    <FormLabel>{field.name}</FormLabel>
-                    {/* Render Input or Select based on field type */}
-                    {field.dataType === "string" ? (
-                      <Input
-                        type="text"
-                        placeholder={`Enter ${field.name}`}
-                        value={fields[field.name] as string}
-                        onChange={(e) => handleChange(e, field.name)}
-                      />
-                    ) : field.dataType === "number" ? (
-                      <Input
-                        type="number"
-                        placeholder={`Enter ${field.name}`}
-                        value={fields[field.name] as number}
-                        onChange={(e) => handleChange(e, field.name)}
-                      />
-                    ) : field.dataType === "boolean" ? (
-                      <Select
-                        value={fields[field.name] as string}
-                        onChange={(e) => handleChange(e, field.name)}
-                        placeholder="Select value"
-                      >
-                        <option value="true">True</option>
-                        <option value="false">False</option>
-                      </Select>
-                    ) : field.dataType === "address" ||
-                      field.dataType === "cid" ? (
-                      <Input
-                        type="text"
-                        placeholder={`Enter ${field.name}`}
-                        value={fields[field.name] as string}
-                        onChange={(e) => handleChange(e, field.name)}
-                      />
-                    ) : field.dataType === "bytes32" ? (
-                      <Input
-                        type="text"
-                        placeholder={`Enter ${field.name} (32-byte string)`}
-                        value={fields[field.name] as string}
-                        onChange={(e) => handleChange(e, field.name)}
-                      />
-                    ) : null}
-                  </FormControl>
-                ))}
+                {schema.fields.map((field: SchemaField) =>
+                  field.name && field.dataType ? (
+                    <FormControl key={field.name} mb={4} isRequired>
+                      <FormLabel>{field.name}</FormLabel>
+                      {/* Render Input or Select based on field type */}
+                      {field.dataType === "string" ? (
+                        <Input
+                          type="text"
+                          placeholder={`Enter ${field.name}`}
+                          value={fields[field.name] as string}
+                          onChange={(e) => handleChange(e, field.name)}
+                          borderColor="gray.500"
+                          bg="white"
+                          _placeholder={{ color: "gray.500" }}
+                          _focus={{ borderColor: "brand.primary" }}
+                          _focusVisible={{ outline: "none" }}
+                        />
+                      ) : field.dataType === "number" ? (
+                        <Input
+                          type="number"
+                          placeholder={`Enter ${field.name}`}
+                          value={fields[field.name] as number}
+                          onChange={(e) => handleChange(e, field.name)}
+                          borderColor="gray.500"
+                          bg="white"
+                          _placeholder={{ color: "gray.500" }}
+                          _focus={{ borderColor: "brand.primary" }}
+                          _focusVisible={{ outline: "none" }}
+                        />
+                      ) : field.dataType === "boolean" ? (
+                        <Select
+                          value={fields[field.name] as string}
+                          onChange={(e) => handleChange(e, field.name)}
+                          placeholder="Select value"
+                          borderColor="gray.500"
+                          bg="white"
+                          _placeholder={{ color: "gray.500" }}
+                          _focus={{ borderColor: "brand.primary" }}
+                          _focusVisible={{ outline: "none" }}
+                        >
+                          <option value="true">True</option>
+                          <option value="false">False</option>
+                        </Select>
+                      ) : field.dataType === "address" ||
+                        field.dataType === "cid" ? (
+                        <>
+                          <Input
+                            type="text"
+                            placeholder="CID will appear here"
+                            value={fields[field.name] as string}
+                            isReadOnly
+                            borderColor="gray.500"
+                            bg="white"
+                            _placeholder={{ color: "gray.500" }}
+                            _focus={{ borderColor: "brand.primary" }}
+                            _focusVisible={{ outline: "none" }}
+                          />
+
+                          <Button
+                            mt={2}
+                            w={"120px"}
+                            _hover={{
+                              backgroundColor: "brand.primary",
+                              color: "white",
+                            }}
+                            onClick={() =>
+                              document
+
+                                .getElementById(`${field.name}-file-input`)
+
+                                ?.click()
+                            }
+                            isDisabled={uploading}
+                          >
+                            {uploading ? <Spinner size="sm" /> : "Upload File"}
+                          </Button>
+
+                          <Input
+                            id={`${field.name}-file-input`}
+                            type="file"
+                            hidden
+                            onChange={(e) => {
+                              if (e.target.files?.[0])
+                                handleFileUpload(e.target.files[0], field.name);
+                            }}
+                            borderColor="gray.500"
+                            bg="white"
+                            _placeholder={{ color: "gray.500" }}
+                            _focus={{ borderColor: "brand.primary" }}
+                            _focusVisible={{ outline: "none" }}
+                          />
+                        </>
+                      ) : field.dataType === "bytes32" ? (
+                        <Input
+                          type="text"
+                          placeholder={`Enter ${field.name} (32-byte string)`}
+                          value={fields[field.name] as string}
+                          onChange={(e) => handleChange(e, field.name)}
+                          borderColor="gray.500"
+                          bg="white"
+                          _placeholder={{ color: "gray.500" }}
+                          _focus={{ borderColor: "brand.primary" }}
+                          _focusVisible={{ outline: "none" }}
+                        />
+                      ) : null}
+                    </FormControl>
+                  ) : null
+                )}
 
                 {/* Button to submit the form */}
                 <Button
